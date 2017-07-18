50ac9547e05835d77191d4178c80945157d4f0af
Reyn-L <Reyn.Leong@gmail.com>
2017-07-18 11:57:19 -1000

    fixed

--- server.js
+++ server.js
@@ -8,37 +8,29 @@ const server = http.createServer((req, res) => {
 
 //check to see if req.url includes paths
 if(paths.includes(req.url)) {
-
     //makes the / path return index.html
     if(req.url === "/") {
       req.url = "/index.html";
     }
-
     //reads the public folder and adds the req.url
     fs.readFile('./public' + req.url, 'utf8', (err, data) => {
-
       //chesks if method is GET
       if(req.method === "GET") {
         fs.readFile('./public' + req.url, (err, data) => {
           console.log(data.toString());
           res.end(data.toString());
         });
-
       //checks if POST
     } else if (req.method === "POST") {
-
       //parse body
       let queryUrl = querystring.parse('application/x-www-form-urlencoded');
-
       //writes files, creates new file if nonexistent
       fs.open('./public' + req.url, 'w', (err, fd) => {
-
         res.end();
       });
 
     }
   });
-
     //if req.url !== paths show this
   } else {
     fs.readFile('./public/404.html', (err, data) => {
