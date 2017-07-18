33c8acb2c2fb81286bda4ed1d6592ede65a1463b
Reyn-L <Reyn.Leong@gmail.com>
2017-07-18 11:56:05 -1000

    fixed spacing

--- server.js
+++ server.js
@@ -5,24 +5,31 @@ const querystring = require('querystring');
 
 const server = http.createServer((req, res) => {
   let paths = ["/", "/index.html", "/hydrogen.html", "/helium.html", "/404.html", '/css/styles.css', 'elements.html'];
+
 //check to see if req.url includes paths
 if(paths.includes(req.url)) {
+
     //makes the / path return index.html
     if(req.url === "/") {
       req.url = "/index.html";
     }
+
     //reads the public folder and adds the req.url
     fs.readFile('./public' + req.url, 'utf8', (err, data) => {
+
       //chesks if method is GET
       if(req.method === "GET") {
         fs.readFile('./public' + req.url, (err, data) => {
           console.log(data.toString());
           res.end(data.toString());
         });
+
       //checks if POST
     } else if (req.method === "POST") {
+
       //parse body
       let queryUrl = querystring.parse('application/x-www-form-urlencoded');
+
       //writes files, creates new file if nonexistent
       fs.open('./public' + req.url, 'w', (err, fd) => {
 
@@ -31,6 +38,7 @@ if(paths.includes(req.url)) {
 
     }
   });
+
     //if req.url !== paths show this
   } else {
     fs.readFile('./public/404.html', (err, data) => {
